760f4241bfbe5907ed34d3f51d5ec494a6420b0d
RUCHIR04-R <ruchirgawture@gmail.com>
2025-01-31 19:00:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+console.log("javaScript is running!");
+	const heading = document.querySelector(h1);
+	heading.addEventListener('click', function(p) {
+		heading.textContent = "You clicked the heading";
+	});
+});
